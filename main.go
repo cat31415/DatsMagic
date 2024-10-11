b9f03d9160cd711b57869f598fe73a0983f62ed4
Kirill Ivanov <kirik20052017@gmail.com>
2024-10-11 17:52:49 +0300

    Start

--- main.go
+++ main.go
@@ -1,4 +1,34 @@
-safa
+package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-dsghfdjf
+type Data struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func main() {
+	// Отправка GET запроса
+	resp, err := http.Get("http://example.com/data.json")
+	if err != nil {
+		fmt.Println("Ошибка при отправке запроса:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Декодирование JSON
+	var data Data
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		fmt.Println("Ошибка при декодировании JSON:", err)
+		return
+	}
+
+	// Использование данных
+	fmt.Println("Ключ:", data.Key)
+	fmt.Println("Значение:", data.Value)
+}
